9cf6f5333f85e2da6667622141b375e95f590d57
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-01 11:24:57 +0600

    duplicate remove with for loop and set difference clear

---
+++ set.js
@@ -0,0 +1,16 @@
+const arr = [1, 2, 2, 3, 3, 4, 4];
+
+// const removeDuplicate = (arr) => {
+//     let newArr = []
+//     for (let i = 0; i < arr.length; i++) {
+//         if (!newArr.includes(arr[i])) {
+//             newArr.push(arr[i])
+//         }
+//     }
+//     return newArr
+// }
+
+// console.log(removeDuplicate(arr))
+
+const set = new Set(arr);
+console.log(set);
